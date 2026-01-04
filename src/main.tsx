17ba5e14a1af205c0260640c643cccc8fec97266
mcqeems <mcqeemsofficial@gmail.com>
2026-01-05 00:35:08 +0700

    add routes for other pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import Home from '@/pages/home/Home.tsx';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Layout from '@/pages/layout/Layout';
+import Profil from '@/pages/profil/Profil';
+import CeritaInspirasi from '@/pages/cerita-inspirasi/CeritaInspirasi';
+import Mitra from '@/pages/mitra/Mitra';
+import Program from '@/pages/program/Program';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +18,22 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: '/profil',
+        element: <Profil />,
+      },
+      {
+        path: '/cerita-inspirasi',
+        element: <CeritaInspirasi />,
+      },
+      {
+        path: '/mitra',
+        element: <Mitra />,
+      },
+      {
+        path: '/program',
+        element: <Program />,
       },
     ],
   },
