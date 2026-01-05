582ce65eafda62b7bb340a322a12eec044eefadb
mcqeems <mcqeemsofficial@gmail.com>
2026-01-05 09:54:32 +0700

    fix scroll position to top in every route change

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export default function Layout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="min-h-dvh flex flex-col">
       <header>
